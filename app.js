633f195fda6ba308b871bf9182af47ae42d34c78
lintaohai <3396205754@qq.com>
2018-09-26 14:36:21 +0800

    https remove

--- app.js
+++ app.js
@@ -3,30 +3,29 @@ const static = require('koa-static')
 const router = require('./router/index')
 const authCheck = require('./middle/auth-check')
 const corsConfig = require('./middle/cors-config')
-const http = require('http')
-const https = require('https')
-const fs = require('fs')
-const enforceHttps = require('koa-sslify')
+// const http = require('http')
+// const https = require('https')
+// const fs = require('fs')
+// const enforceHttps = require('koa-sslify')
 
 let app = new Koa()
 
 app
-.use(enforceHttps())
+// .use(enforceHttps())
 .use(static('assets'))     // 静态资源
 .use(static('www'))        // 文件资源
 .use(corsConfig)           // 跨越设置
 // .use(authCheck)            // 登录校验
 .use(router)               // 路由
 
-// app.listen(9000, '0.0.0.0', function () {
-//   console.log(new Date(), 'server running http://0.0.0.0:9000');
-// });
+app.listen(9000, '0.0.0.0', function () {
+  console.log(new Date(), 'server running http://0.0.0.0:9000');
+});
 
-let options = {
-  key: fs.readFileSync('./ssl/1539869715215.key'),
-  cert: fs.readFileSync('./ssl/1539869715215.pem')
-}
+// let options = {
+//   key: fs.readFileSync('./ssl/1539869715215.key'),
+//   cert: fs.readFileSync('./ssl/1539869715215.pem')
+// }
  
-// start the server
-http.createServer(app.callback()).listen(8000);
-https.createServer(options, app.callback()).listen(443);
+// http.createServer(app.callback()).listen(9000);
+// https.createServer(options, app.callback()).listen(443);
