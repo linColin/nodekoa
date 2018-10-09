9e5a73f1bf0f0a00ae0d52def222984949a2ce9d
lintaohai <3396205754@qq.com>
2018-10-09 17:56:42 +0800

    sql 修改

--- db/d-resume.js
+++ db/d-resume.js
@@ -4,10 +4,11 @@ const Sequelize = require('sequelize')
 
 
 async function getMsg(key) {
-  let sqlStr = `SELECT * FROM users INNER JOIN user_flows ON users.id=user_flows.id and users.key = ${key};`
+  let sqlStr = `SELECT a.name, a.age, a.gender, a.mobile, a.email, a.addr, a.hukou, a.avatar_url, b.pro, b.skill FROM users as a INNER JOIN user_flows as b ON a.id=b.id and a.key = ${key};`
   return await Model.query(sqlStr, { type: Model.QueryTypes.SELECT})
 }
 
 module.exports = {
   getMsg
 }
+, a.age, a.gender, a.mobile, a.email, a.addr, a.hukou, a.avatar_url, b.pro, b.skill
